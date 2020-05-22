6e1942051c42ea2fa523fdf91a3a1be3c6342876
carlos <carlosgiovani@gmail.com>
2020-05-22 10:34:01 -0300

    Tela de meus pedidos feitos

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
@@ -8,7 +9,29 @@ class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(orderId),
+      margin: EdgeInsets.symmetric(vertical: 1, horizontal: 1),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: StreamBuilder<DocumentSnapshot>(
+          stream: Firestore.instance.collection("orders").document(orderId).snapshots(),
+          // ignore: missing_return
+          builder: (context, snapshot){
+            if(!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            else {
+              return Column(
+                children: [
+                  Text(
+                    "Código do pedido: ${snapshot.data.documentID}",
+                  )
+                ],
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
